6a18cb1fdb3e4598b85a2947397df43cf4f68d9e
jeanbravo359 <yans8360@gmail.com>
2020-06-08 05:21:14 -0500

    lpcom

---
+++ operaciones.js
@@ -0,0 +1,7 @@
+var x , y ;
+             x=9;
+             y=6;
+            document.getElementById('suma').innerHTML="El resultado de la suma es : " + (x+y);
+            document.getElementById('resta').innerHTML="El resultado de la resta es : " + (x-y);    
+            document.getElementById('multiplicacion').innerHTML="El resultado de la multiplicacion es : " + (x/y);    
+            document.getElementById('division').innerHTML="El resultado de la divisicion  es : " + (x/y); 
